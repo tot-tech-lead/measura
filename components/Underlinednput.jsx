d45862d965f43072cee27ba919ab30fb77cd1ed5
VitaliiDTS <150488372+VitaliiDTS@users.noreply.github.com>
2024-12-12 18:29:32 +0200

    add UnderlinedInput component

---
+++ components/Underlinednput.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useRef, useEffect } from "react";
+import { View, TextInput, StyleSheet, Animated } from "react-native";
+import Txt from "./Text";
+
+// до inputType обирати такі значення (default, numeric, decimal-pad)
+//                               (простий текст, цілі числа, десяткові числа)
+
+export default function InputField({ value, setValue, label, inputType }) {
+    const [isFocused, setIsFocused] = useState(false);
+    const borderColorAnim = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+
+        Animated.timing(borderColorAnim, {
+            toValue: isFocused ? 1 : 0,
+            duration: 60,
+            useNativeDriver: false,
+        }).start();
+    }, [isFocused, borderColorAnim]);
+
+
+    const borderBottomColor = borderColorAnim.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["#ccc", "#000"],
+    });
+
+    return (
+        <View style={styles.container}>
+            <Txt
+                style={{
+                    fontSize: 12,
+                    fontFamily: "GeologicaRegular",
+                    fontWeight: "400",
+                    marginBottom: 4,
+                    marginLeft: 5,
+                }}
+            >
+                {label}
+            </Txt>
+            <Animated.View style={{ borderBottomColor, borderBottomWidth: 1 }}>
+                <TextInput
+                    style={styles.input}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    placeholder=""
+                    placeholderTextColor="#ccc"
+                    value={value}
+                    onChangeText={setValue}
+                    keyboardType={inputType}
+                />
+            </Animated.View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    input: {
+        fontSize: 15,
+        paddingVertical: 5,
+        color: "#000",
+    },
+});
