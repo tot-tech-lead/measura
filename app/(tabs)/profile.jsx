fed150d6e44a2d9931fde1782796cde31f9af8b8
Viktor Kindrat <kindratvictor07@gmail.com>
2024-12-12 20:17:03 +0200

    Setup 'In development' message on Profile screen

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -5,7 +5,10 @@ const Profile = () => {
     return (
         <ViewWithBackground style={{padding: 18, paddingTop: 36, flex: 1, justifyContent: 'center' }}>
             <Txt style={{fontWeight: "500", fontSize: 32, textAlign: "center", color: "#595959"}}>
-                Go to profile.jsx to edit this page
+                Функціонал у розробці ⚙️
+            </Txt>
+            <Txt style={{fontWeight: "400", fontSize: 16, textAlign: "center", color: "#595959"}}>
+                Навідайтесь після оновлення
             </Txt>
         </ViewWithBackground>
     )
