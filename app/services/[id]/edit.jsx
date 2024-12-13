2b7da6a410e72286f4e775b634ebeaec8aabba2b
Viktor Kindrat <kindratvictor07@gmail.com>
2024-12-13 21:02:16 +0200

    Fix form bugs

--- app/services/[id]/edit.jsx
+++ app/services/[id]/edit.jsx
@@ -156,6 +156,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         flexDirection: "column",
         gap: 25,
+        zIndex: 1,
     },
     formContainer: {
         width: "100%",
--- app/services/create.jsx
+++ app/services/create.jsx
@@ -144,6 +144,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         flexDirection: "column",
         gap: 25,
+        zIndex: 1,
     },
     formContainer: {
         width: "100%",
