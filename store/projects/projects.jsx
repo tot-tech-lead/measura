3146fb680957ef3596fd00affd8c5c8501e72276
Viktor Kindrat <kindratvictor07@gmail.com>
2024-12-16 22:23:31 +0200

    Creation logic setup

---
+++ store/projects/projects.jsx
@@ -0,0 +1,36 @@
+import 'react-native-get-random-values';
+import {createSlice} from "@reduxjs/toolkit";
+import {v4 as uuidv4} from 'uuid';
+
+const initialState = {
+    projects: new Array(0)
+};
+
+const additionalServiceSlice = createSlice({
+    name: "projects",
+    initialState,
+    reducers: {
+        addNew: (state, action) => {
+            const uniqueId = uuidv4();
+            state.projects.push({
+                ...action.payload,
+                id: uniqueId,
+            });
+        },
+        deleteOne: (state, action) => {
+            return {
+                projects: state.projects.filter(item => item.id !== action.payload),
+            };
+        },
+        editOne: (state, action) => {
+            return {
+                projects: state.projects.map(item =>
+                    item.id === action.payload.id ? {...item, ...action.payload} : item
+                )
+            };
+        },
+    },
+});
+
+export const {addNew, deleteOne, editOne} = additionalServiceSlice.actions;
+export default additionalServiceSlice.reducer;
