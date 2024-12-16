5fa8d2f5330940472a28ef9f2b2eafe1272ecea3
VitaliiDTS <150488372+VitaliiDTS@users.noreply.github.com>
2024-12-16 22:38:50 +0200

    add part of  math functions

--- app/projects/[id]/view.jsx
+++ app/projects/[id]/view.jsx
@@ -1,17 +1,39 @@
-import {StyleSheet} from "react-native";
+import {Image, ScrollView, StyleSheet,Text, View} from "react-native";
 import {useLocalSearchParams} from "expo-router";
 
 import ViewWithDoubleBackground from "../../../components/ViewWithDoubleBackground";
 import Headline from "../../../components/Headline";
 
+
+
 export default function ViewProject() {
     let {id} = useLocalSearchParams()
 
+
+
+
     return (
         <ViewWithDoubleBackground>
-            <Headline>View project with id {id}</Headline>
+            <Image style={styles.image}></Image>
+            <ScrollView style={styles.container}>
+                <Headline>{id}</Headline>
+
+            </ScrollView>
+
+
         </ViewWithDoubleBackground>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        gap: 25
+    },
+    image: {
+        width: "100%",
+        height: 200,
+        resizeMode: "cover",
+
+    },
+});
