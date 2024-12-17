e75977f660599f23bdbacac9ee8aaacd17e9e07a
Viktor Kindrat <kindratvictor07@gmail.com>
2024-12-17 23:11:06 +0200

    fix edit page

--- components/projects/Stage2.jsx
+++ components/projects/Stage2.jsx
@@ -7,23 +7,23 @@ export default function Stage2({data, setProperty}) {
     return (
         <View style={styles.container}>
             <Headline>Матеріали</Headline>
-            <UnderlinedInput value={String(data?.tileWidth) || ""}
+            <UnderlinedInput value={data?.tileWidth ? String(data?.tileWidth) : ""}
                              setValue={(v) => setProperty("tileWidth", v)}
                              label={"Ширина плитки (мм.)"}
                              inputType="numeric" />
-            <UnderlinedInput value={String(data?.tileHeight) || ""}
+            <UnderlinedInput value={data?.tileHeight ? String(data?.tileHeight) : ""}
                              setValue={(v) => setProperty("tileHeight", v)}
                              label={"Довжина плитки (мм.)"}
                              inputType="numeric" />
-            <UnderlinedInput value={String(data?.tileCostForMeterSq) || ""}
+            <UnderlinedInput value={data?.tileCostForMeterSq ? String(data?.tileCostForMeterSq) : ""}
                              setValue={(v) => setProperty("tileCostForMeterSq", v)}
                              label={"Вартість за 1 м²"}
                              inputType="numeric" />
-            <UnderlinedInput value={String(data?.gluePrice) || ""}
+            <UnderlinedInput value={data?.gluePrice ? String(data?.gluePrice) : ""}
                              setValue={(v) => setProperty("gluePrice", v)}
                              label={"Вартість мішка клею"}
                              inputType="numeric" />
-            <UnderlinedInput value={String(data?.glueWeight) || ""}
+            <UnderlinedInput value={data?.glueWeight ? String(data?.glueWeight) : ""}
                              setValue={(v) => setProperty("glueWeight", v)}
                              label={"Маса мішка (кг)"}
                              inputType="numeric" />
