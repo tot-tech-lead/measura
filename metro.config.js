ae4396ef683c736ec91d1577cc97cab06a1f383d
VitaliiDTS <150488372+VitaliiDTS@users.noreply.github.com>
2025-03-28 10:02:48 +0200

    add metro.config.js in addition to tsconfig.json, now metro bundler recognize @path/path routing

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot);
+
+config.resolver.alias = {
+  ...config.resolver.alias,
+  '@components': path.resolve(projectRoot, 'components'),
+  '@lib': path.resolve(projectRoot, 'lib'),
+  '@images': path.resolve(projectRoot, 'assets/images'),
+  '@fonts': path.resolve(projectRoot, 'assets/fonts'),
+  '@svg': path.resolve(projectRoot, 'assets/svg'),
+  '@app': path.resolve(projectRoot, 'app'),
+  '@store': path.resolve(projectRoot, 'store'),
+};
+module.exports = config;
